2a8ee9acb9d7beb5f8053f835ea7e7679b0d50af
santosh33 <santosh33@gmail.com>
2022-05-11 18:11:43 +0530

    Added Coupon Entity for Discount

---
+++ src/Services/Discount/Discount.API/Entites/Coupon.cs
@@ -0,0 +1,13 @@
+﻿namespace Discount.API.Entites
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Description { get; set; }
+
+        public string Amount { get; set; }
+    }
+}
