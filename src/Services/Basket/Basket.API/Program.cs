8da307b6916f5bd45eb6ccbbbb11ef2ec859b97c
santosh33 <santosh33@gmail.com>
2022-05-13 05:10:43 +0530

    Added comments in Progam.cs

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,15 +1,23 @@
+using Basket.API.GrpServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Redis service
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-    //options.Configuration = "localhost:6379";
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
+//Register Dependency injected classes
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+//Register DiscountGrpcService and Client
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+    (options => options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+builder.Services.AddScoped<DiscountGrpcService>();
+
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
