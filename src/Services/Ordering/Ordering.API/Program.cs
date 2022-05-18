d4f5e35f1f1bb794d88f7fdec3b91487e1fca3e7
santosh33 <santosh33@gmail.com>
2022-05-18 21:45:11 +0530

    Develop Application Service Extensions

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,11 +1,16 @@
+using Ordering.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Adding Application Service Extensions
+builder.Services.AddApplicationServices();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
