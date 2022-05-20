248fe28b5320fb63311f6e2869e5b84b28d1bcba
santosh33 <santosh33@gmail.com>
2022-05-20 19:39:34 +0530

    Applying EF Migration to SQL Server for Ordering Microservice

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,7 @@
+using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -17,6 +19,13 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+//Migrate SQLServer database when Ordering Microservices Startup
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
